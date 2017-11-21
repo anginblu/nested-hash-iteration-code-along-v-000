2f3a72bf676c2a0bcbf05ab5f09f66d63f41e15c
Jacqueline Lee <anginblu@gmail.com>
2017-11-21 04:53:55 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,19 +15,7 @@ require 'pry'
   # }
 
 def remove_strawberry(contacts)
-  contacts =
-      {
-        "Jon Snow" => {
-          name: "Jon",
-          email: "jon_snow@thewall.we",
-          favorite_ice_cream_flavors: ["chocolate", "vanilla"]
-        },
-        "Freddy Mercury" => {
-          name: "Freddy",
-          email: "freddy@mercury.com",
-          favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
-        }
-      }
+  binding.pry
 
   contacts.each do |name, data|
     data.each do |attribute, value|
